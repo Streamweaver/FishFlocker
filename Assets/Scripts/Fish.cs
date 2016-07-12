5926a0763d4a5e15ed02bf7e3bd164af212aea21
Scott Turnbull <streamweaver@gmail.com>
2016-07-12 19:29:10 -0400

    Initial Commit

---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fish : MonoBehaviour {
+
+	public float speed;
+	public float turnSpeed = 4.0f;
+	Vector3 averageHeading;
+	Vector3 averagePosition;
+	float neighborDistance = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range (0.5f, 1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Random.Range (1, 5) < 2)
+			ApplyRules ();
+
+		transform.Translate (0, 0, Time.deltaTime * speed);
+	}
+
+	void ApplyRules() {
+		GameObject[] gos;
+		gos = GlobalFlock.allFish;
+
+		Vector3 vCenter = Vector3.zero;
+		Vector3 vAvoid = Vector3.zero;
+		float gSpeed = 0.1f;
+
+		Vector3 goalPos = GlobalFlock.goalPos;
+
+		float dist;
+		int groupSize = 0;
+
+		foreach (GameObject go in gos) {
+			if (go != this.gameObject) {
+				dist = Vector3.Distance (go.transform.position, this.transform.position);
+				if (dist <= neighborDistance) {
+					vCenter += go.transform.position;
+					groupSize++;
+
+					if(dist < 1.0f) {
+						vAvoid += (this.transform.position - go.transform.position);
+					}
+
+					Fish anotherFish = go.GetComponent<Fish> ();
+					gSpeed += anotherFish.speed;
+				}
+
+			}
+		}
+
+		if (groupSize > 0) {
+			vCenter = vCenter / groupSize + (goalPos - this.transform.position);
+			speed = gSpeed / groupSize;
+
+			Vector3 direction = (vCenter + vAvoid) - transform.position;
+			if (direction != Vector3.zero) {
+				transform.rotation = Quaternion.Slerp (transform.rotation,
+					Quaternion.LookRotation (direction),
+					turnSpeed * Time.deltaTime);
+			}
+		}
+
+ 	}
+ }
---
+++ Assets/Scripts/GlobalFlock.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalFlock : MonoBehaviour {
+
+	public GameObject fishPrefab;
+	public GameObject fishSchool;
+	public static int tankSize = 5;
+
+	static int numFish = 10;
+	public static GameObject[] allFish = new GameObject[numFish];
+	public static Vector3 goalPos = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < numFish; i++) {
+			Vector3 pos = new Vector3 (
+				Random.Range(-tankSize, tankSize),
+				Random.Range(-tankSize, tankSize),
+				Random.Range(-tankSize, tankSize)
+			);
+			GameObject fish = (GameObject)Instantiate (fishPrefab, pos, Quaternion.identity);
+			fish.transform.parent = fishSchool.transform;
+			allFish [i] = fish;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HandleGoalPos ();
+	}
+
+	void HandleGoalPos() {
+		if (Random.Range(1, 1000) < 50) {
+			goalPos = new Vector3 (
+				Random.Range(-tankSize, tankSize),
+				Random.Range(-tankSize, tankSize),
+				Random.Range(-tankSize, tankSize)
+			);
+		}
+	}
+}
