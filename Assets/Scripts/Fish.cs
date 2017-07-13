761d290d632a01588cc4e8226da5234ea6e33efe
Dario D. Müller <dario-domi@hotmail.de>
2017-07-13 16:33:23 +0200

    ADDED bug-free, clean-coded, performance-optimized and more setup-able version of fish flocking with dynamic target

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -1,93 +1,129 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class Fish : MonoBehaviour {
+public class Fish : MonoBehaviour
+{
+    public GlobalFlock GlobalFlock { get; set; }
+    public float speed;
+    Vector3 averageHeading;
+    Vector3 averagePosition;
+    private GameObject[] _allFish;
+    private Vector3 myPosition;
+    private int myInstanceID;
 
-	public float speed;
-	//public float turnSpeed = 4.0f;
-	Vector3 averageHeading;
-	Vector3 averagePosition;
-	float neighborDistance = 3.0f;
+    void Start()
+    {
+        _allFish = GlobalFlock.AllFish;
+        speed = GetRandomSpeed();
+        myInstanceID = this.GetInstanceID();
+    }
 
-	bool turning = false;
+    void Update()
+    {
+        myPosition = this.transform.position;
+        var doTurning = ApplyTankBoundary();
 
-	// Use this for initialization
-	void Start () {
-		speed = Random.Range (0.5f, 1.0f);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		ApplyTankBoundary ();
+        if (doTurning)
+        {
+            DoTurning();
+        }
+        else if (Random.Range(0, 5) < 1)
+        {
+            ApplyRules();
+        }
 
-		if(turning) {
-			Vector3 direction = Vector3.zero - transform.position;
-			transform.rotation = Quaternion.Slerp (transform.rotation,
-				Quaternion.LookRotation (direction),
-				TurnSpeed () * Time.deltaTime);
-			speed = Random.Range (0.5f, 1);
-		} else {
-			if (Random.Range (0, 5) < 1)
-				ApplyRules ();
-		}
+        transform.Translate(0, 0, Time.deltaTime * speed);
+    }
 
-		transform.Translate (0, 0, Time.deltaTime * speed);
-	}
+    private void DoTurning()
+    {
+        Vector3 direction = Vector3.zero - myPosition;
+        var a = transform.rotation;
+        var b = Quaternion.LookRotation(direction);
+        var t = TurnSpeed() * Time.deltaTime;
+        transform.rotation = Quaternion.Slerp(a, b, t);
+        speed = GetRandomSpeed();
+    }
 
-	void ApplyTankBoundary() {
-		if(Vector3.Distance(transform.position, Vector3.zero) >= GlobalFlock.tankSize) {
-			turning = true;
-		} else {
-			turning = false;
-		}
-	}
+    private float GetRandomSpeed()
+    {
+        return Random.Range(GlobalFlock.FishMinSpeed, GlobalFlock.FishMaxSpeed);
+    }
 
-	void ApplyRules() {
-		GameObject[] gos;
-		gos = GlobalFlock.allFish;
+    private bool ApplyTankBoundary()
+    {
+        var dist = Vector3.Distance(myPosition, GetGoalPos());
+        var tankSize = GlobalFlock.TankSize;
+        return dist < tankSize;
+    }
 
-		Vector3 vCenter = Vector3.zero;
-		Vector3 vAvoid = Vector3.zero;
-		float gSpeed = 0.1f;
+    private void ApplyRules()
+    {
 
-		Vector3 goalPos = GlobalFlock.goalPos;
+        Vector3 vCenter = Vector3.zero;
+        Vector3 vAvoid = Vector3.zero;
+        float gSpeed = 0;
 
-		float dist;
-		int groupSize = 0;
+        Vector3 goalPos = GetGoalPos();
 
-		foreach (GameObject go in gos) {
-			if (go != this.gameObject) {
-				dist = Vector3.Distance (go.transform.position, this.transform.position);
-				if (dist <= neighborDistance) {
-					vCenter += go.transform.position;
-					groupSize++;
+        float dist;
+        int groupSize = 0;
 
-					if(dist < 0.75f) {
-						vAvoid = vAvoid + (this.transform.position - go.transform.position);
-					}
+        for (var i = 0; i < _allFish.Length; i++)
+        {
+            GameObject fish = _allFish[i];
 
-					Fish anotherFish = go.GetComponent<Fish> ();
-					gSpeed += anotherFish.speed;
-				}
+            if (fish.GetInstanceID() == myInstanceID)
+                continue;
 
-			}
-		}
+            var otherFishPos = fish.transform.position;
+            dist = Vector3.Distance(otherFishPos, myPosition);
+            if (dist <= GlobalFlock.FishNeighborDistance)
+            {
+                vCenter += otherFishPos;
+                groupSize++;
 
-		if (groupSize > 0) {
-			vCenter = vCenter / groupSize + (goalPos - this.transform.position);
-			speed = gSpeed / groupSize;
+                if (dist <= GlobalFlock.FishAvoidDistance)
+                {
+                    vAvoid += myPosition - otherFishPos;
+                }
 
-			Vector3 direction = (vCenter + vAvoid) - transform.position;
-			if (direction != Vector3.zero) {
-				transform.rotation = Quaternion.Slerp (transform.rotation,
-					Quaternion.LookRotation (direction),
-					TurnSpeed () * Time.deltaTime);
-			}
-		}
+                Fish anotherFish = fish.GetComponent<Fish>();
+                gSpeed += anotherFish.speed;
+            }
+        }
 
- 	}
+        Vector3 diff;
+        if (groupSize > 0)
+        {
+            diff = vCenter / groupSize + (goalPos - myPosition);
+            speed = gSpeed / groupSize;
+        }
+        else
+        {
+            diff = goalPos - myPosition;
+            speed = GetRandomSpeed();
+        }
 
-	float TurnSpeed() {
-		return Random.Range (0.2f, 0.6f);
-	}
- }
+        Vector3 direction = (diff + vAvoid) - myPosition;
+
+        if (direction == Vector3.zero)
+            return;
+
+        var a = transform.rotation;
+        var b = Quaternion.LookRotation(direction);
+        var t = TurnSpeed() * Time.deltaTime;
+        transform.rotation = Quaternion.Slerp(a, b, t);
+
+    }
+
+    private Vector3 GetGoalPos()
+    {
+        return GlobalFlock.goalPositon;
+    }
+
+    private float TurnSpeed()
+    {
+        return Random.Range(0.2f, 0.6f) * GlobalFlock.FishTurnSpeed;
+    }
+
+}
